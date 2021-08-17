c24793be278eb2ccde0ab2ef460cff6674d17ab4
Crowdbotics <team@crowdbotics.com>
2021-08-17 20:25:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "supermarket_deals_29891Identifier",
+  url: "https://supermarket_deals_29891Identifier.botics.co",
+  api: "https://supermarket_deals_29891Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
